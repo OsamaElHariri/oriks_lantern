5437e07ff28cb159a8f5c3b44400aea6fecd4434
Osama El Hariri <yamsandbread@gmail.com>
2020-09-15 23:56:15 +0300

    Add experiments with light

--- gameplay/world/level_origin/LevelOrigin.gd
+++ gameplay/world/level_origin/LevelOrigin.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
+var glow = preload("res://gameplay/world/level_origin/glow.png")
+
 var grass = [
 	preload("res://gameplay/world/level_origin/grass1.png"),
 	preload("res://gameplay/world/level_origin/grass2.png"),
 	preload("res://gameplay/world/level_origin/grass3.png"),
-	preload("res://gameplay/world/level_origin/mushroom1.png"),
-	preload("res://gameplay/world/level_origin/mushroom2.png"),
+]
+
+var plants = [
 	preload("res://gameplay/world/level_origin/plant1.png"),
 	preload("res://gameplay/world/level_origin/plant2.png"),
+]
+
+var mushroom = [
+	preload("res://gameplay/world/level_origin/mushroom1.png"),
+	preload("res://gameplay/world/level_origin/mushroom2.png"),
 ]
 var top_cell_coords = []
 
@@ -20,20 +28,64 @@ func _ready():
 		$TileMap.update_bitmask_region()
 		for pos in top_cell_coords:
 			var auto_tile_coord = $TileMap.get_cell_autotile_coord(pos.x, pos.y)
-			if (auto_tile_coord == Vector2(1, 0) \
-				or auto_tile_coord == Vector2(1, 5)) \
-				and randf() < 0.6:
-				var sprite = Sprite.new()
-				sprite.texture = get_random_texture()
+			var is_top_cell = auto_tile_coord == Vector2(1, 0) or auto_tile_coord == Vector2(1, 5)
+			if is_top_cell and randf() < 0.6:
+				var sprite = spawn_grass()
 				sprite.position = $TileMap.map_to_world(pos)
-				sprite.z_index = 10
-				sprite.offset = Vector2(sprite.offset.x, -sprite.texture.get_size().y / 1.95)
-				sprite.scale = Vector2(sign(randf() - 0.5), 1.2 - randf() * 0.4)
-				sprite.rotation_degrees = 3 - 6 * randf()
-				
-				var modulate_multiplier = 0.9 - randf() * 0.1
-				sprite.modulate = Color(modulate_multiplier, modulate_multiplier, modulate_multiplier, 1)
-				add_child(sprite)
+			if is_top_cell and randf() < 0.5:
+				var sprite = spawn_plant() if randf() < 0.5 else spawn_mushroom()
+				sprite.position = $TileMap.map_to_world(pos)
+
+func spawn_grass():
+	var sprite = Sprite.new()
+	sprite.texture = grass[randi() % grass.size()]
+	sprite.z_index = 10
+	sprite.offset = Vector2(sprite.offset.x, -sprite.texture.get_size().y / 1.95)
+	sprite.scale = Vector2(sign(randf() - 0.5), 1.2 - randf() * 0.4)
+	sprite.rotation_degrees = 3 - 6 * randf()
+	
+	var modulate_multiplier = 0.9 - randf() * 0.1
+	sprite.modulate = Color(modulate_multiplier, modulate_multiplier, modulate_multiplier, 1)
+	add_child(sprite)
+	return sprite
+
+func spawn_plant():
+	var sprite = Sprite.new()
+	sprite.texture = plants[randi() % plants.size()]
+	sprite.z_index = 10
+	sprite.offset = Vector2(sprite.offset.x, -sprite.texture.get_size().y / 1.95)
+	sprite.scale = Vector2(sign(randf() - 0.5), 1.2 - randf() * 0.4)
+	sprite.rotation_degrees = 3 - 6 * randf()
+	
+	var modulate_multiplier = 0.9 - randf() * 0.1
+	sprite.modulate = Color(modulate_multiplier, modulate_multiplier, modulate_multiplier, 1)
+	add_child(sprite)
+	var light = Light2D.new()
+	sprite.add_child(light)
+	light.texture = glow
+	light.color = Color(0.9, 0.2, 0.38, 1)
+	light.position = Vector2(0, -sprite.texture.get_size().y)
+	light.energy = 1.5
+	return sprite
+
+func spawn_mushroom():
+	var sprite = Sprite.new()
+	sprite.texture = mushroom[randi() % mushroom.size()]
+	sprite.z_index = 10
+	sprite.offset = Vector2(sprite.offset.x, -sprite.texture.get_size().y / 1.95)
+	sprite.scale = Vector2(sign(randf() - 0.5), 1.2 - randf() * 0.4)
+	sprite.rotation_degrees = 3 - 6 * randf()
+	
+	var modulate_multiplier = 0.9 - randf() * 0.1
+	sprite.modulate = Color(modulate_multiplier, modulate_multiplier, modulate_multiplier, 1)
+	add_child(sprite)
+	var light = Light2D.new()
+	sprite.add_child(light)
+	light.texture = glow
+	light.position = Vector2(0, -sprite.texture.get_size().y / 2)
+	light.color = Color(0.2, 0.6, 0.53, 1)
+	light.energy = 1.2
+	return sprite
 
 func draw_platform_in_tilemap(platform):
 	var platform_size = platform.texture.get_size()
@@ -50,6 +102,3 @@ func draw_platform_in_tilemap(platform):
 			$TileMap.set_cell(map_pos.x, map_pos.y, 0)
 			current_x += $TileMap.cell_size.x
 		current_y += $TileMap.cell_size.y
-
-func get_random_texture():
-	return grass[randi() % grass.size()]
